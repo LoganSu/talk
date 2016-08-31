8d9fcae42487c2060a9e718ed0588cd68833ac20
pengjingyu <188518533@qq.com>
2016-08-31 17:30:28 +0800

    修改文件生成MD5值的时候把开头的0去掉的问题

--- src/main/java/com/youlb/utils/common/SHAEncrypt.java
+++ src/main/java/com/youlb/utils/common/SHAEncrypt.java
@@ -123,7 +123,7 @@ public class SHAEncrypt {
 	    byte[] uploadBytes = upload.getBytes();
 	    MessageDigest md5 = MessageDigest.getInstance("MD5");
 	    byte[] digest = md5.digest(uploadBytes);
-	    String hashString = new BigInteger(1, digest).toString(16);
+	    String hashString = DecodUtils.bytesToHexString(digest);
 	    return hashString.toUpperCase();
 	}
 	
